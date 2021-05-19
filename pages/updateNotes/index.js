6dcc312806f605d02cd544981ce2f085e992c3a4
sohan2410 <bandarysohan24@gmail.com>
2021-05-19 12:07:09 +0530

    commit 2

---
+++ pages/updateNotes/index.js
@@ -0,0 +1,10 @@
+const createNoteInput = document.querySelector(".create-note-input");
+
+createNoteInput.addEventListener("input", (e) => {
+  console.log(e.target.value);
+});
+
+const urlParams = new URLSearchParams(window.location.search);
+const noteId = urlParams.get("noteId");
+
+console.log(noteId);
